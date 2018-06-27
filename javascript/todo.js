2cb05ce14c4da27f281b66c465cac63a5496977b
Tyler Plasman <tylerplasman@gmail.com>
2018-06-26 22:05:46 -0800

    made javascript todo list for website

---
+++ javascript/todo.js
@@ -0,0 +1,37 @@
+
+//create a new list item when clicking the "add" button
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue == '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("myList").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (var i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+
+
+//close button to hide current list item
+var close = document.getElementsByClassName("close");
+for (var i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
