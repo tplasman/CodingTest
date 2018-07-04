9df78d3f88ff9b08f066252ff1095431d4981ee5
Tyler Plasman <tylerplasman@gmail.com>
2018-07-03 18:36:57 -0800

    moving around JS

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+var rolls;
+
+function diceRoll() {
+    var dice = document.getElementByID("sides");
+    var sides = document.getElementByID("dice");
+
+    Math.floor(Math.random() * sides) +1;
+ 
+
+return rolls
+
+}
+
+console.log(rolls);
