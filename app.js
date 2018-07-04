81c87f1250aa6aaf9d2c7a5d07134eb47aeae68a
Tyler Plasman <35716253+tplasman@users.noreply.github.com>
2018-07-03 18:20:14 -0700

    dice roll web app

---
+++ app.js
@@ -0,0 +1,12 @@
+var rolls;
+
+function diceRoll(){
+
+  var dice = document.getElementByID("sides");
+  var sides = document.getElementByID("dice");
+
+  Math.floor(Math.random() * sides) +1;
+
+return rolls
+
+}
