f09d6fd82616ea01c5480d60d4b4dd1cacf91d2b
Tyler Plasman <tylerplasman@gmail.com>
2018-07-08 21:57:55 -0700

    added statement to prevent rolls with now dice

--- javascript/app.js
+++ javascript/app.js
@@ -8,10 +8,17 @@ function rollDice(sides) {
     //get amount of dice from user
     var amount = document.getElementById("amount").value;
 
-    for(var i = 0; i < amount; i++){
-        //random number generator
-        var rolls = 1 + Math.floor(Math.random() * sides);
-        sum += rolls;
+    //reguiring values to work or giving alert error
+    if (sides > 0 && amount > 0) {
+        for (var i = 0; i < amount; i++) {
+            //random number generator
+            var rolls = 1 + Math.floor(Math.random() * sides);
+            sum += rolls;
+        }
+    } else if (sides <= 0) {
+        alert("Enter how many sides on the dice!")
+    } else {
+        alert("Enter how many dice are being rolled!")
     }
 
     //update the UI
